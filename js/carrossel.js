7f902499645e424669f440345cbc1d175151cce9
RaulFerreiraSilva <raul.etec.2019@gmail.com>
2022-10-16 18:42:06 -0300

    arrumando o carrossel

---
+++ js/carrossel.js
@@ -0,0 +1,13 @@
+$('.carrossel').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  cssEase: 'linear',
+  prevArrow: false,
+  nextArrow: false
+});
